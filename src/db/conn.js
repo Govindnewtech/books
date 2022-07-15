66434a8954ce4344f6013aebf37d926bb7c57ae4
Govindnewtech <107402563+Govindnewtech@users.noreply.github.com>
2022-07-15 12:48:01 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 // const url = "mongodb://localhost:27017/stationery";
 
 
-// const url = 'mongodb+srv://aakash:akki8817@pushtak.pyht9.mongodb.net/Pushtak?retryWrites=true&w=majority';
-const url = 'mongodb+srv://govind_kewat:8ndbaL1uxYHWTDJy@cluster0.qegpu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+ const url = 'mongodb+srv://aakash:akki8817@pushtak.pyht9.mongodb.net/Pushtak?retryWrites=true&w=majority';
+// const url = 'mongodb+srv://govind_kewat:8ndbaL1uxYHWTDJy@cluster0.qegpu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 // const url = 'mongodb+srv://Pushtak:lkc9CeUYpBElTZMj@cluster0.6eadk.mongodb.net/?retryWrites=true&w=majority';
 
 
