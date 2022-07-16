8fde27a21970cbb27a833a28886d41dd99c0446f
Govindnewtech <107402563+Govindnewtech@users.noreply.github.com>
2022-07-16 15:10:30 +0530

    Update conn.js

--- src/db/conn.js
+++ src/db/conn.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 
 
 // const url = 'mongodb+srv://aakash:akki8817@pushtak.pyht9.mongodb.net/Pushtak?retryWrites=true&w=majority';
-const url = 'mongodb+srv://govind_kewat:8ndbaL1uxYHWTDJy@cluster0.qegpu.mongodb.net/Cluster0?retryWrites=true&w=majority';
+const url = 'mongodb+srv://admin:ntf12345@cluster0.slwmg.mongodb.net/?retryWrites=true&w=majority'
 
 mongoose.connect(url, {
     useNewUrlParser: true, useUnifiedTopology: true,
