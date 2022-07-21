42dd92178572dddefd5e9f9a7aca701e961cab80
Govindnewtech <107402563+Govindnewtech@users.noreply.github.com>
2022-07-21 13:49:48 +0530

    Update category.js

--- src/router/category.js
+++ src/router/category.js
@@ -36,19 +36,18 @@ router.post("/category/add", async (req, res) => {
 
     const categories = await Category.find({ name: name });
 
-   if (categories.length == 0) {
+    if (categories.length == 0) {
       const public = './public/'
-      const Fpath = 'https://book-api-ntf.herokuapp.com/public/uploads/books/images/' + new Date().getTime().toString() + '.jpg'
-      fs.writeFileSync(Fpath, front, { encoding: 'base64' })
-
+      const Fpath = 'uploads/category/' + new Date().getTime().toString() + '.jpg'
+      fs.writeFileSync(public + Fpath, front, { encoding: 'base64' })
       const newCategory = new Category({
-        userId, name, description, front: Fpath
+        userId, name, description, front: APIURL + Fpath
       });
 
 
       const savedCategory = await newCategory.save();
       return res.status(200).json(savedCategory);
-    }
+    } 
     else {
       return res.status(200).json({
         'code': 201,
